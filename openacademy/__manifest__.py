ca1aa6728e0d5af8c7ca426cfc303ac8c861cac8
belisac <belisa@vauxoo.com>
2018-11-20 19:01:56 +0000

    REF] openacademy: Add tree, form and search

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -19,12 +19,13 @@
     'data': [
     # 'security/ir.model.access.csv',
     #'views/views.xml',
-    'views/templates.xml',
     'views/openacademy.xml',
+    'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
     'demo/demo.xml',
     ],
     'application': True,
-}
+    'installable': True,
+   }
