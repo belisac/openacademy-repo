bf32465c2fd74331419cbbb8e6632d453e4b0a82
belisac <belisa@vauxoo.com>
2018-11-29 18:38:59 +0000

    REF openacademy: Add board

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -13,7 +13,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -24,6 +24,7 @@
         'views/templates.xml',
         'views/partner.xml',
         'reports.xml',
+        'views/session_board.xml'
         ],
     # only loaded in demonstration mode
     'demo': [
