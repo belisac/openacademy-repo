0ca1240d52e77ed68e7e1fd24c8e163bb09facf0
belisac <belisa@vauxoo.com>
2018-12-10 18:11:24 +0000

    FIX Openacademy - Flake8 manifest.py and wizard.py

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -7,8 +7,8 @@
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/
+    # base/data/ir_module_category_data.xml  for the full list
     'category': 'Test',
     'version': '0.1',
 
@@ -19,7 +19,7 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
-        #'views/views.xml',
+        # 'views/views.xml'
         'views/openacademy.xml',
         'views/templates.xml',
         'views/partner.xml',
@@ -28,8 +28,7 @@
         ],
     # only loaded in demonstration mode
     'demo': [
-    'demo/demo.xml',
-    ],
+    'demo/demo.xml',],
     'application': True,
     'installable': True,
-   }
+}
