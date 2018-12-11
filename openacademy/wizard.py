a7d6a65766c2e712445025d58b9b9715be0bfb09
belisac <belisa@vauxoo.com>
2018-12-11 14:55:34 +0000

    REF wizard and parent .py indented

--- openacademy/wizard.py
+++ openacademy/wizard.py
@@ -13,8 +13,8 @@ class Wizard(models.TransientModel):
         return session_records
 
     session_ids = fields.Many2many(
-            'openacademy.session', requiered=True,
-            default=_default_session)
+                'openacademy.session', requiered=True,
+                default=_default_session)
     attendee_ids = fields.Many2many('res.partner')
 
     @api.multi
